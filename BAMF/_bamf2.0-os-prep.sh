a59bcb7cf10fb6ac82b00a2e5fc4a4f4b2ebdaa5
BOC Business Solutions <35924867+bocbusinesssolutions@users.noreply.github.com>
2018-09-24 19:51:41 -0400

    Delete _bamf2.0-os-prep.sh

--- BAMF/_bamf2.0-os-prep.sh
+++
@@ -1,9 +0,0 @@
-# Docker should be installed
-# Docker should be running
-systemctl status docker
-yum -y install git
-yum -y install unzip
-iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
-iptables -A INPUT -p tcp --dport 443 -j ACCEPT
-iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
